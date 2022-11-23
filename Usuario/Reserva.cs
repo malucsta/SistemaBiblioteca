8743cac257923a9c177f68da826d46ee0fc7e227
vinicius <vinicius.silva@farmaciasapp.com.br>
2022-11-23 15:07:17 -0300

    fix: reserva was empty hehe

--- Usuario/Reserva.cs
+++ Usuario/Reserva.cs
@@ -2,6 +2,9 @@ namespace SistemaBiblioteca.Usuario
 {
 public class Reserva 
 {
+    public string codigoLivro { get; set; }
+    public string codigoUsuario { get; set; }
+    public Date solicitacaoData { get; set; }
 
 }
 
